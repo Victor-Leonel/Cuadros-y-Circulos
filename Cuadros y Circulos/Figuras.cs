d344d149689c0b6f0fbc0fb0a1d2c98a7e42c4ae
VL <vlab.134031@gmail.com>
2016-04-13 10:42:13 +0200

    Primera Vercion
    Lo volbi a subir por que no lo habia creado  sin git ignore visual
    studio

---
+++ Cuadros y Circulos/Figuras.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Cuadros_y_Circulos
+{
+    abstract class Figuras
+    {
+        public int X, Y;
+        public Pen pluma;
+        public Brush brocha;
+        public int ancho, largo;
+
+        public Figuras(int x, int y)
+        {
+            X = x;
+            Y = y;
+            pluma = new Pen(Color.Red, 2);
+            ancho = 10;
+            largo = 10;
+
+        }
+        public abstract void Dibuja(Form f);
+    }
+    class Rectangulo : Figuras
+    {
+        public Rectangulo(int x, int y) : base(x, y)
+        {
+
+        }
+        public override void Dibuja(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawRectangle(pluma, X, Y, ancho, largo);
+        }
+    }
+
+    class Circulo : Figuras
+    {
+        public Circulo(int x, int y) : base(x, y)
+        {
+
+        }
+        public override void Dibuja(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawEllipse(pluma, X, Y, ancho, largo);
+        }
+    }
+}
---
+++ Cuadros y Circulos/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuadros_y_Circulos
+{
+    public partial class Form1 : Form
+    {
+        List<Figuras> figuras;
+
+        public Form1()
+        {
+            figuras = new List<Figuras>();
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender,EventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Circulo c = new Circulo(e.X, e.Y);
+                c.Dibuja(this);
+                figuras.Add(c);
+
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Rectangulo r = new Rectangulo(e.X, e.Y);
+                r.Dibuja(this);
+                figuras.Add(r);
+            }
+        }
+    }
+}
